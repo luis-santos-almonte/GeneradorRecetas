2a63d1490410d8d06a023d4a330e59d27e853a9c
luis-santos-almonte <luisreynaldosa@gmail.com>
2025-03-15 00:22:14 -0300

    Implementing root

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from tkinter import Button, Label, Tk
+from tkinter import Button, Label
 from lib.classes import patient, prescription
 
-class App(Tk):
-    def __init__(self):
-        Tk.__init__(self)
-        self.title("My App")
-        self.geometry("300x200")
-        self.resizable(False, False)
-        self.label = Label(self, text="Hello, World!")
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Hello, World!")
+        self.root.geometry("300x200")
+        self.root.resizable(False, False)
+        self.label = Label(self.root, text="Hello, World!")
         self.label.pack()
-        self.button = Button(self, text="Click Me", command=self.on_click)
+        self.button = Button(self.root, text="Click Me", command=self.on_click)
         self.button.pack()
 
     def on_click(self):
--- init.py
+++ init.py
@@ -1,5 +1,7 @@
+import tkinter as tk
 from app import App
 
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
