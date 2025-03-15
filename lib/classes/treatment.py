752b7b767a9f58668c9f2cb716791fda40688f5f
luis-santos-almonte <luisreynaldosa@gmail.com>
2025-03-14 23:29:29 -0300

    Creating treatment class

---
+++ lib/classes/treatment.py
@@ -0,0 +1,5 @@
+class treatment:
+    def __init__(self, description: str, usage: str, quantity: int):
+        self.description = description
+        self.usage = usage
+        self.quantity = quantity
