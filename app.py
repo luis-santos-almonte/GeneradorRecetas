47eddf105b95f297490746817248c26b0136dd0e
luis-santos-almonte <luisreynaldosa@gmail.com>
2025-03-15 00:48:40 -0300

    Creating patient entries

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from tkinter import Button, Label
+from tkinter import Button, Label, Entry, messagebox
 from lib.classes import patient, prescription
 
 class App:
     def __init__(self, root):
         self.root = root
         self.root.title("Hello, World!")
-        self.root.geometry("300x200")
+        self.root.geometry("800x600")
         self.root.resizable(False, False)
-        self.label = Label(self.root, text="Hello, World!")
-        self.label.pack()
-        self.button = Button(self.root, text="Click Me", command=self.on_click)
-        self.button.pack()
-
-    def on_click(self):
-        self.label["text"] = "Button Clicked"
+        
+        Label(self.root, text="Nombre:").grid(row=0, column=0, padx=10, pady=10)
+        self.entry_name = Entry(self.root)
+        self.entry_name.grid(row=0, column=1, padx=10, pady=10)
+        
+        Label(self.root, text="Apellido:").grid(row=1, column=0, padx=10, pady=10)
+        self.entry_lastname = Entry(self.root)
+        self.entry_lastname.grid(row=1, column=1, padx=10, pady=10)
+        
+        Label(self.root, text="Edad:").grid(row=3, column=0, padx=10, pady=10)
+        self.entry_age = Entry(self.root)
+        self.entry_age.grid(row=3, column=1, padx=10, pady=10)
+        
+        self.boton_agregar = Button(self.root, text="Agregar")
+        self.boton_agregar.grid(row=4, column=1, columnspan=2, pady=10)
+        
+        self.boton_agregar = Button(self.root, text="Imprimir")
+        self.boton_agregar.grid(row=4, column=2, columnspan=2, pady=10)
